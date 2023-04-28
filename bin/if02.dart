83aba13ec788f5079db0ac221ace916a01a287ef
Mirjalol <jalolubaydullayev575@gmail.com>
2023-04-28 14:04:06 +0500

    02

--- bin/if02.dart
+++ bin/if02.dart
@@ -8,5 +8,20 @@ Args:
 Returns:
     int: return answer.
 */
-void main() {
+int func(int a, int b ,int c) { 
+    int answer = 0; 
+if (a < b && a < c) { 
+    return answer = a; 
+} 
+if ( b < a && b < c){ 
+    return answer + b;
+} else { 
+    if ( c < a && c < b) { 
+    return answer + c; 
+}; 
+}
+return answer;
+} 
+void main() { 
+    print(func(4, 14, 3)); 
 }
